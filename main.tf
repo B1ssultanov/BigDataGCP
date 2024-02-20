5af13345e4aae7ac49da217373fb0994a72c7a25
B1ssultanov <95929680+B1ssultanov@users.noreply.github.com>
2024-02-21 00:49:56 +0600

    Terraform configuration

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.16.0"
+    }
+  }
+}
+
+provider "google" {
+#   credentials = "./keys/keys/fiery-topic-414915-0dd7735c4180.json"
+  project     = "fiery-topic-414915"
+  region      = "us-central1"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  name          = "yedyge_bucket_check"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
